39db4fea529c3e70ef900c5056bea64c94794346
MainlyMaya <Superjussi2009@gmail.com>
2026-03-15 19:52:05 +0100

    attempt1

---
+++ src/main/java/net/colormaya/labyrinths_of_loot/LabyrinthsOfLoot.java
@@ -0,0 +1,16 @@
+package net.colormaya.labyrinths_of_loot;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LabyrinthsOfLoot implements ModInitializer {
+	public static final String MOD_ID = "labyrinths_of_loot";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/colormaya/labyrinths_of_loot/LabyrinthsOfLootClient.java
@@ -0,0 +1,10 @@
+package net.colormaya.labyrinths_of_loot;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LabyrinthsOfLootClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
