5bfc6a271690161d0f8af4825c7581c1af95259b
Colordiasply <colordisply@gmail.com>
2026-03-16 17:05:50 -0400

    new shit

---
+++ src/main/java/net/colormaya/labyrinths_of_loot/block/ModBlocks.java
@@ -0,0 +1,41 @@
+package net.colormaya.labyrinths_of_loot.block;
+
+import net.colormaya.labyrinths_of_loot.LabyrinthsOfLoot;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.MapColor;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+    public class ModBlocks {
+        public static final Block LIMESTONE_BRICKS = registerBlock("limestone_bricks",
+                new Block(AbstractBlock.Settings.create().requiresTool().strength(1.5F, 2.0F).sounds(BlockSoundGroup.DRIPSTONE_BLOCK).mapColor(MapColor.TERRACOTTA_BROWN)));
+        public static final Block CHISELED_LIMESTONE_BRICKS = registerBlock("chiseled_limestone_bricks",
+                new Block(AbstractBlock.Settings.create().requiresTool().strength(1.5F, 2.0F).sounds(BlockSoundGroup.DRIPSTONE_BLOCK).mapColor(MapColor.TERRACOTTA_BROWN)));
+
+        private static Block registerBlock(String name, Block block) {
+            registerBlockItem(name, block);
+            return Registry.register(Registries.BLOCK, Identifier.of(LabyrinthsOfLoot.MOD_ID, name), block);
+        }
+
+        private static void registerBlockItem(String name, Block block) {
+            Registry.register(Registries.ITEM, Identifier.of(LabyrinthsOfLoot.MOD_ID, name),
+                    new BlockItem(block, new Item.Settings()));
+        }
+
+        public static void registerModBlocks() {
+            LabyrinthsOfLoot.LOGGER.info("Registering Mod Blocks for " + LabyrinthsOfLoot.MOD_ID);
+
+            ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+                entries.add(LIMESTONE_BRICKS);
+                entries.add(CHISELED_LIMESTONE_BRICKS);
+
+            });
+        }
+    }
---
+++ src/main/java/net/colormaya/labyrinths_of_loot/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.colormaya.labyrinths_of_loot.item;
+
+import net.colormaya.labyrinths_of_loot.LabyrinthsOfLoot;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
+
+    public class ModItems {
+        public static final Item LIMESTONE_CHUNK = registerItem("limestone_chunk", new Item(new Item.Settings()));
+
+        private static Item registerItem(String name, Item item) {
+            return Registry.register(Registries.ITEM, Identifier.of(LabyrinthsOfLoot.MOD_ID, name), item);
+        }
+
+        public static void registerModItems() {
+            LabyrinthsOfLoot.LOGGER.info("Registering Mod Items for " + LabyrinthsOfLoot.MOD_ID);
+
+            ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+                entries.add(LIMESTONE_CHUNK);
+
+            });
+        }
+    }
+
