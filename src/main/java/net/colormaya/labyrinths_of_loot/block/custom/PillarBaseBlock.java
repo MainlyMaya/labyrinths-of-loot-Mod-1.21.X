ed3b33ced33918920c976483ca8a2bec31cfa4cd
MainlyMaya <Superjussi2009@gmail.com>
2026-03-17 19:54:18 +0100

    Update-005
    > added
    Mire Brick
    Coral Fragments
    Coralstone Pillars
    + Base Pillar

---
+++ src/main/java/net/colormaya/labyrinths_of_loot/block/custom/PillarBaseBlock.java
@@ -0,0 +1,42 @@
+package net.colormaya.labyrinths_of_loot.block.custom;
+
+import com.mojang.serialization.MapCodec;
+import net.minecraft.block.*;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.Direction;
+
+public class PillarBaseBlock extends FacingBlock {
+    public PillarBaseBlock(AbstractBlock.Settings settings) {
+        super(settings);
+        this.setDefaultState(this.stateManager.getDefaultState().with(FACING, Direction.SOUTH));
+    }
+
+    @Override
+    protected MapCodec<? extends FacingBlock> getCodec() {
+        return null;
+    }
+
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Override
+    protected BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    protected BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite().getOpposite());
+    }
+}
